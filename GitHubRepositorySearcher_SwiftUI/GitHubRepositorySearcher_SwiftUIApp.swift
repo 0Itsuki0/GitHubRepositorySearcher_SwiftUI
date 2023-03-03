c0708cdd0a82ae79cd21c26b4ff1fe54036f4374
0Itsuki0 <2018.itsuki@gmail.com>
2023-03-03 16:25:24 +0900

    Initial Commit

---
+++ GitHubRepositorySearcher_SwiftUI/GitHubRepositorySearcher_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositorySearcher_SwiftUIApp.swift
+//  GitHubRepositorySearcher_SwiftUI
+//
+//  Created by イツキ on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepositorySearcher_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
